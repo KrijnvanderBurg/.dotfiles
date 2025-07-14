4483ca5c7b3e36e7d9f3099667fc1d62794dca93
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-07-14 19:49:17 +0000

    fix install check for pytest

--- python/scripts/pytest_and_coverage.sh
+++ python/scripts/pytest_and_coverage.sh
@@ -7,10 +7,9 @@ output_coverage_filepath="${5:-$PWD/coverage.xml}" && echo "Coverage output will
 output_junit_filepath="${6:-$PWD/JUNIT-TEST.xml}" && echo "Pytest junit output will be saved to: $output_junit_filepath"
 
 # Install pytest and related packages if not already installed
-if ! command -v pytest &> /dev/null; then
-    echo "Installing pytest..."
-    pip install pytest --quiet
-    pip install pytest-cov pytest-xdist --quiet
+if ! command -v pytest &> /dev/null || ! python3 -c "import pytest_cov" &> /dev/null || ! python3 -c "import xdist" &> /dev/null; then
+    echo "Installing pytest and related packages..."
+    pip3 install pytest pytest-cov pytest-xdist --quiet
 fi
 
 pytest "$tests_path" \
